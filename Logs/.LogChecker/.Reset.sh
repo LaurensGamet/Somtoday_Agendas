ceedc150f51547f2e10402e57fefff2ccb9c2d76
LaurensGamet <73340705+LaurensGamet@users.noreply.github.com>
2025-01-19 18:02:08 +0100

    180219012025

--- Logs/.LogChecker/.Reset.sh
+++ Logs/.LogChecker/.Reset.sh
@@ -1,9 +1,9 @@
-sudo rm /home/laurens/logs/Somtoday_Laurens.log /home/laurens/logs/Somtoday_Madelief.log
+sudo rm /home/laurens/Somtoday_Agendas/Logs/Laurens.log /home/laurens/Somtoday_Agendas/Logs/Madelief.log
 
-sudo touch /home/laurens/logs/Somtoday_Laurens.log
-sudo touch /home/laurens/logs/Somtoday_Madelief.log
+sudo touch /home/laurens/Somtoday_Agendas/Logs/Laurens.log
+sudo touch /home/laurens/Somtoday_Agendas/Logs/Madelief.log
 
-sudo bash /home/laurens/logs/.LogChecker/Restart.sh
+sudo bash /home/laurens/Somtoday_Agendas/Logs/.LogChecker/Restart.sh
 
-sudo bash /home/laurens/Somtoday_Laurens/run.sh >> /home/laurens/logs/Somtoday_Laurens.log 2>&1
-sudo bash /home/laurens/Somtoday_Madelief/run.sh >> /home/laurens/logs/Somtoday_Madelief.log 2>&1
+sudo bash /home/laurens/Somtoday_Agendas/Laurens/run.sh >> /home/laurens/Somtoday_Agendas/Logs/Laurens.log 2>&1
+sudo bash /home/laurens/Somtoday_Agendas/Madelief/run.sh >> /home/laurens/Somtoday_Agendas/Logs/Madelief.log 2>&1
