8b437f27a9537886f13e328ac0febdcca3f4a556
LaurensGamet <73340705+LaurensGamet@users.noreply.github.com>
2025-01-19 18:14:17 +0100

    181419012025

--- Logs/.LogChecker/Autostart.sh
+++ Logs/.LogChecker/Autostart.sh
@@ -1,17 +1,29 @@
-tmux new -d -s Somtoday_Laurens
-tmux send-keys -t Somtoday_Laurens "clear" C-m 
-tmux send-keys -t Somtoday_Laurens "cd /home/laurens/Somtoday_Agendas/Logs/.LogChecker/Checkers" C-m 
-tmux send-keys -t Somtoday_Laurens "sudo python3 Somtoday_Laurens.py" C-m
-tmux new -d -s Somtoday_Madelief
-tmux send-keys -t Somtoday_Madelief "clear" C-m 
-tmux send-keys -t Somtoday_Madelief "cd /home/laurens/Somtoday_Agendas/Logs/.LogChecker/Checkers" C-m 
-tmux send-keys -t Somtoday_Madelief "sudo python3 Somtoday_Madelief.py" C-m
-tmux new -d -s httpserver
-tmux send-keys -t httpserver "clear" C-m
-tmux send-keys -t httpserver "cd /home/laurens/Somtoday_Agendas/Logs/" C-m
-tmux send-keys -t httpserver "sudo python3 -m http.server" C-m
-tmux new -d -s autologgitupdater
-tmux send-keys -t autologgitupdater "clear" C-m
-tmux send-keys -t autologgitupdater "cd /home/laurens/Somtoday_Agendas/Logs" C-m
-tmux send-keys -t autologgitupdater "sudo python3 .autoupdategit.py" C-m
+tmux new -d -s Somtoday_Agendas -n Laurens
+tmux new-window -t Somtoday_Agendas:1 -n Madelief
+tmux new-window -t Somtoday_Agendas:2 -n httpserver
+tmux new-window -t Somtoday_Agendas:3 -n autologggitupdater
+tmux split-window -h -t Somtoday_Agendas:0
+tmux split-window -v -t Somtoday_Agendas:0.0
+tmux split-window -v -t Somtoday_Agendas:0.1
+tmux select-layout -t Somtoday_Agendas:0 tiled
+tmux kill-window -t Test:1
+tmux kill-window -t Test:2
+
+
+
+tmux send-keys -t Somtoday_Agendas:0 "clear" C-m 
+tmux send-keys -t Somtoday_Agendas:0 "cd /home/laurens/Somtoday_Agendas/Logs/.LogChecker/Checkers" C-m 
+tmux send-keys -t Somtoday_Agendas:0 "sudo python3 Somtoday_Laurens.py" C-m
+
+tmux send-keys -t Somtoday_Agendas:1 "clear" C-m 
+tmux send-keys -t Somtoday_Agendas:1 "cd /home/laurens/Somtoday_Agendas/Logs/.LogChecker/Checkers" C-m 
+tmux send-keys -t Somtoday_Agendas:1 "sudo python3 Somtoday_Madelief.py" C-m
+
+tmux send-keys -t Somtoday_Agendas:2 "clear" C-m
+tmux send-keys -t Somtoday_Agendas:2 "cd /home/laurens/Somtoday_Agendas/Logs/" C-m
+tmux send-keys -t Somtoday_Agendas:2 "sudo python3 -m http.server" C-m
+
+tmux send-keys -t Somtoday_Agendas:3 "clear" C-m
+tmux send-keys -t Somtoday_Agendas:3 "cd /home/laurens/Somtoday_Agendas/Logs" C-m
+tmux send-keys -t Somtoday_Agendas:3 "sudo python3 .autoupdategit.py" C-m
 
