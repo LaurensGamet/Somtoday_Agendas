9cd11dd99b4117684848e20769f675933d7c2493
LaurensGamet <73340705+LaurensGamet@users.noreply.github.com>
2025-01-19 19:37:32 +0100

    193719012025

--- Logs/.LogChecker/Autostart.sh
+++ Logs/.LogChecker/Autostart.sh
@@ -5,19 +5,12 @@ tmux split-window -v -t Somtoday_Agendas:0.1
 tmux select-layout -t Somtoday_Agendas:0 tiled
 
 
-tmux send-keys -t Somtoday_Agendas:0.0 "clear" C-m 
-tmux send-keys -t Somtoday_Agendas:0.0 "cd /home/laurens/Somtoday_Agendas/Logs/.LogChecker/Checkers" C-m 
-tmux send-keys -t Somtoday_Agendas:0.0 "sudo python3 Somtoday_Laurens.py" C-m
+tmux send-keys -t Somtoday_Agendas:0.0 "sudo python3 /home/laurens/Somtoday_Agendas/Logs/.LogChecker/Checkers/Somtoday_Laurens.py" C-m
 
-tmux send-keys -t Somtoday_Agendas:0.1 "clear" C-m 
-tmux send-keys -t Somtoday_Agendas:0.1 "cd /home/laurens/Somtoday_Agendas/Logs/.LogChecker/Checkers" C-m 
-tmux send-keys -t Somtoday_Agendas:0.1 "sudo python3 Somtoday_Madelief.py" C-m
+tmux send-keys -t Somtoday_Agendas:0.1 "sudo python3 /home/laurens/Somtoday_Agendas/Logs/.LogChecker/Checkers/Somtoday_Madelief.py" C-m 
 
-tmux send-keys -t Somtoday_Agendas:0.2 "clear" C-m
 tmux send-keys -t Somtoday_Agendas:0.2 "cd /home/laurens/Somtoday_Agendas/Logs/" C-m
 tmux send-keys -t Somtoday_Agendas:0.2 "sudo python3 -m http.server" C-m
 
-tmux send-keys -t Somtoday_Agendas:0.3 "clear" C-m
-tmux send-keys -t Somtoday_Agendas:0.3 "cd /home/laurens/Somtoday_Agendas/Logs" C-m
-tmux send-keys -t Somtoday_Agendas:0.3 "sudo python3 .autoupdategit.py" C-m
+tmux send-keys -t Somtoday_Agendas:0.3 "sudo python3 /home/laurens/Somtoday_Agendas/.autoupdategit.py" C-m
 
