551fdb42c123f568a4734f258509eb1fed098aed
LaurensGamet <73340705+LaurensGamet@users.noreply.github.com>
2025-01-19 20:07:13 +0100

    200719012025

--- Madelief/run.sh
+++ Madelief/run.sh
@@ -9,12 +9,13 @@ cd /home/laurens/Somtoday_Agendas/Madelief
 echo
 
 echo "# Get up to date"
-sudo git pull --rebase
+sudo git pull
 
 echo
 
 echo "# Run main file"
 sudo python3 /home/laurens/Somtoday_Agendas/Madelief/main.py
+echo "# Done"
 
 echo  
 echo  
