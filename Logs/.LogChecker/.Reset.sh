d5076694ce3e34085137740046050f9920176fc3
RPI-Botzen <laurensbotzen3@gmail.com>
2025-01-19 22:33:16 +0100

    223319012025

--- Logs/.LogChecker/.Reset.sh
+++ Logs/.LogChecker/.Reset.sh
@@ -4,6 +4,3 @@ sudo touch /home/laurens/Somtoday_Agendas/Logs/Laurens.log
 sudo touch /home/laurens/Somtoday_Agendas/Logs/Madelief.log
 
 sudo bash /home/laurens/Somtoday_Agendas/Logs/.LogChecker/Restart.sh
-
-sudo bash /home/laurens/Somtoday_Agendas/Laurens/run.sh >> /home/laurens/Somtoday_Agendas/Logs/Laurens.log 2>&1
-sudo bash /home/laurens/Somtoday_Agendas/Madelief/run.sh >> /home/laurens/Somtoday_Agendas/Logs/Madelief.log 2>&1
