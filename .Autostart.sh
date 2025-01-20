1d33fd1f3913353bc721ececa113a173f9251be7
LaurensGamet <73340705+LaurensGamet@users.noreply.github.com>
2025-01-20 20:46:03 +0100

    204520012025

--- .Autostart.sh
+++ .Autostart.sh
@@ -5,9 +5,9 @@ tmux split-window -v -t Somtoday_Agendas:0.1
 tmux select-layout -t Somtoday_Agendas:0 tiled
 
 
-tmux send-keys -t Somtoday_Agendas:0.0 "sudo python3 /home/laurens/Somtoday_Agendas/Logs/.LogChecker/Checkers/Somtoday_Laurens.py" C-m
+tmux send-keys -t Somtoday_Agendas:0.0 "sudo python3 /home/laurens/Somtoday_Agendas/Logs/.Checkers/Somtoday_Laurens.py" C-m
 
-tmux send-keys -t Somtoday_Agendas:0.1 "sudo python3 /home/laurens/Somtoday_Agendas/Logs/.LogChecker/Checkers/Somtoday_Madelief.py" C-m 
+tmux send-keys -t Somtoday_Agendas:0.1 "sudo python3 /home/laurens/Somtoday_Agendas/Logs/.Checkers/Somtoday_Madelief.py" C-m 
 
 tmux send-keys -t Somtoday_Agendas:0.2 "cd /home/laurens/Somtoday_Agendas/Logs/" C-m
 tmux send-keys -t Somtoday_Agendas:0.2 "sudo python3 -m http.server" C-m
