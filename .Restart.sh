48c9875c97a92db0026de0ab8a4528bab0e46b6b
RPI-Botzen <laurensbotzen3@gmail.com>
2025-01-20 20:38:50 +0100

    203820012025

---
+++ .Restart.sh
@@ -0,0 +1,3 @@
+tmux kill-ses -t Somtoday_Agendas
+
+sudo bash /home/laurens/Somtoday_Agendas/Autostart.sh
--- Logs/.LogChecker/Restart.sh
+++
@@ -1,3 +0,0 @@
-tmux kill-ses -t Somtoday_Agendas
-
-sudo bash /home/laurens/Somtoday_Agendas/Logs/.LogChecker/Autostart.sh
