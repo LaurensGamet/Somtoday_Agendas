b1fdfa39110b24a8f3acd769956a93017829606b
RPI-Botzen <laurensbotzen3@gmail.com>
2025-01-23 23:50:27 +0100

    235023012025

--- Logs/.Checkers/slackcredentials.py
+++ Logs/.Checkers/slackcredentials.py
@@ -1,1 +1,1 @@
-Incoming_Webhook_URL = https://hooks.slack.com/services/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
+Incoming_Webhook_URL = "https://hooks.slack.com/services/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"
