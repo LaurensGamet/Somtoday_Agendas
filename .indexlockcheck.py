f9b7b179970af680135a7f496a205236745c3299
RPI-Botzen <laurensbotzen3@gmail.com>
2025-02-28 10:36:50 +0100

    103628022025

--- .indexlockcheck.py
+++ .indexlockcheck.py
@@ -1,7 +1,7 @@
 import os
 
 lock_file_path = "/home/laurens/Somtoday_Agendas/.git/index.lock"
-lock_file_path = "/home/laurens/Somtoday_Agendas/.git/HEAD.lock"
+head_file_path = "/home/laurens/Somtoday_Agendas/.git/HEAD.lock"
 if os.path.exists(lock_file_path):
     os.remove(lock_file_path)
 elif os.path.exists(head_file_path):
