ba67bc05235ec55baba37bf106f9daaa2bdc0840
LaurensGamet <laurensgamet@gmail.com>
2025-05-01 22:08:08 +0200

    220501052025

--- .indexlockcheck.py
+++ .indexlockcheck.py
@@ -2,10 +2,10 @@ import os
 
 lock_file_path = "/home/laurens/Somtoday_Agendas/.git/index.lock"
 head_file_path = "/home/laurens/Somtoday_Agendas/.git/HEAD.lock"
-if os.path.exists(lock_file_path):
-    os.remove(lock_file_path)
-elif os.path.exists(head_file_path):
-    os.remove(head_file_path)
+#if os.path.exists(lock_file_path):
+#    os.remove(lock_file_path)
+#elif os.path.exists(head_file_path):
+#    os.remove(head_file_path)
 
 
 try:
