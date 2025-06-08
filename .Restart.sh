f77842a8bc4de4b42e934d9f975f658d799a5aad
RPI-Botzen <laurensbotzen3@gmail.com>
2025-06-08 11:01:23 +0200

    110108062025

--- .Restart.sh
+++ .Restart.sh
@@ -1,7 +1,7 @@
 echo Test
 
-sudo -u www-data sudo tmux kill-session -t Somtoday_Agendas
+sudo -u www-data sudo tmux kill-session -t Somtoday_Agendas && echo test2
 
-sudo -u www-data sudo bash /home/laurens/Somtoday_Agendas/.Autostart.sh
+sudo -u www-data sudo bash /home/laurens/Somtoday_Agendas/.Autostart.sh && echo test3
 
 sudo -u www-data sudo tmux send-keys -t Combined_View:0.0 "sudo tmux a -t Somtoday_Agendas" C-m
