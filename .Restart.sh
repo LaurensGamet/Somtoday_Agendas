ce29f3851a99cf14acc7391b4e267984e0f8149f
RPI-Botzen <laurensbotzen3@gmail.com>
2025-06-08 10:49:51 +0200

    104908062025

--- .Restart.sh
+++ .Restart.sh
@@ -1,5 +1,5 @@
 echo Test
 
-sudo -u root tmux kill-session -t Somtoday_Agendas
+sudo -u www-data sudo tmux kill-session -t Somtoday_Agendas
 
 sudo bash /home/laurens/Somtoday_Agendas/.Autostart.sh
