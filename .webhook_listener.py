f6dfc67cdcbf6f6b08dde7106d8a1863fbbd59f6
LaurensGamet <laurensgamet@gmail.com>
2025-06-21 14:08:13 +0200

    140821062025

--- .webhook_listener.py
+++ .webhook_listener.py
@@ -1,34 +1,27 @@
-# webhook_listener.py
 from flask import Flask, request
 import subprocess
 
 app = Flask(__name__)
 
 @app.route('/reset', methods=['GET', 'POST'])
-def webhook():
-    # You can check headers or payload here if needed
-    subprocess.Popen(["/home/laurens/Somtoday_Agendas/.Reset.sh"])
-    return "Script started", 200
+def reset_script():
+    subprocess.Popen(["sudo", "/home/laurens/Somtoday_Agendas/.Reset.sh"])
+    return "Reset script started", 200
 
 @app.route('/restart', methods=['GET', 'POST'])
-def webhook():
-    # You can check headers or payload here if needed
-    subprocess.Popen(["/home/laurens/Somtoday_Agendas/.Restart.sh"])
-    return "Script started", 200
+def restart_script():
+    subprocess.Popen(["sudo", "/home/laurens/Somtoday_Agendas/.Restart.sh"])
+    return "Restart script started", 200
 
 @app.route('/update', methods=['GET', 'POST'])
-def webhook():
-    # You can check headers or payload here if needed
-    subprocess.Popen(["/home/laurens/Somtoday_Agendas/.run-updater.sh"])
-    return "Script started", 200
+def update_script():
+    subprocess.Popen(["sudo", "/home/laurens/Somtoday_Agendas/.run-updater.sh"])
+    return "Update script started", 200
 
 @app.route('/autostart', methods=['GET', 'POST'])
-def webhook():
-    # You can check headers or payload here if needed
-    subprocess.Popen(["/home/laurens/Somtoday_Agendas/.Autostart.sh"])
-    return "Script started", 200
-
-
+def autostart_script():
+    subprocess.Popen(["sudo", "/home/laurens/Somtoday_Agendas/.Autostart.sh"])
+    return "Autostart script started", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
