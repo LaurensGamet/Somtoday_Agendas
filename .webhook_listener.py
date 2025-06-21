374d39e1f4f7c2c9bd940bf5398b507e5b746019
LaurensGamet <laurensgamet@gmail.com>
2025-06-21 13:49:01 +0200

    134821062025

--- .webhook_listener.py
+++ .webhook_listener.py
@@ -4,7 +4,7 @@ import subprocess
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['GET', 'POST'])
+@app.route('/reset', methods=['GET', 'POST'])
 def webhook():
     # You can check headers or payload here if needed
     subprocess.Popen(["/home/laurens/Somtoday_Agendas/.Reset.sh"])
