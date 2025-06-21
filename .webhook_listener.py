082d57c41a8a6d639f93f17b4eec5a1891ff8f6c
LaurensGamet <laurensgamet@gmail.com>
2025-06-21 14:05:46 +0200

    140521062025

--- .webhook_listener.py
+++ .webhook_listener.py
@@ -10,5 +10,25 @@ def webhook():
     subprocess.Popen(["/home/laurens/Somtoday_Agendas/.Reset.sh"])
     return "Script started", 200
 
+@app.route('/restart', methods=['GET', 'POST'])
+def webhook():
+    # You can check headers or payload here if needed
+    subprocess.Popen(["/home/laurens/Somtoday_Agendas/.Restart.sh"])
+    return "Script started", 200
+
+@app.route('/update', methods=['GET', 'POST'])
+def webhook():
+    # You can check headers or payload here if needed
+    subprocess.Popen(["/home/laurens/Somtoday_Agendas/.run-updater.sh"])
+    return "Script started", 200
+
+@app.route('/autostart', methods=['GET', 'POST'])
+def webhook():
+    # You can check headers or payload here if needed
+    subprocess.Popen(["/home/laurens/Somtoday_Agendas/.Autostart.sh"])
+    return "Script started", 200
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
