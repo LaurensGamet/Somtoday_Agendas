e74a8dc67e406a3b892086757aadc0a796842251
LaurensGamet <73340705+LaurensGamet@users.noreply.github.com>
2025-08-16 00:00:54 +0200

    Update Custom.py

--- Custom.py
+++ Custom.py
@@ -26,6 +26,7 @@ Lokalen = [
 Lessen = {
     f'{klas3}.bl1': 'Begeleidingslessen',
     f'{klas3}.biol1': 'Biologie',
+    f'{klas3}.ckvnieuw1': 'Culturele en Kunstzinnige Vorming',
     f'{klas3}.ckvnieuw2': 'Culturele en Kunstzinnige Vorming',
     f'{klas3}.dutl1': 'Duits',
     f'{klas3}.econ1': 'Economie',
