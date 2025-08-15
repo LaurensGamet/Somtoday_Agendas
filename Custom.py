3c3c9ae368d2cf88f705c05aa74cd2a401044ebe
LaurensGamet <73340705+LaurensGamet@users.noreply.github.com>
2025-08-16 00:20:41 +0200

    Update Custom.py

--- Custom.py
+++ Custom.py
@@ -35,6 +35,7 @@ Lessen = {
     f'{klas3}.fatl1': 'Frans',
     f'{klas1}lo': 'Lichamelijke Opvoeding',
     f'oga4.kua1\, oga5.kua1': 'Kunstgeschiedenis',
+    f'oga5.kua1\, oga4.kua1': 'Kunstgeschiedenis',
     f'oga6.kumu1\, oga4.kumu1\, oga5.kumu1\, oh5.kumu1\, oh4.kumu1': 'Kunst (Muziek)',
     f'{klas1}maat': 'Maatschappijleer',
     f'{klas3}.mentor1': 'Mentor',
