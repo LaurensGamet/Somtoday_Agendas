ca71fc7329eba10b08db204325b4f38f7ee476ce
LaurensGamet <73340705+LaurensGamet@users.noreply.github.com>
2025-08-15 23:33:47 +0200

    Update Custom.py

--- Custom.py
+++ Custom.py
@@ -34,6 +34,7 @@ Lessen = {
     f'{klas1}fa': 'Frans',
     f'{klas1}gs': 'Geschiedenis',
     f'{klas1}lo': 'Lichamelijke Opvoeding',
+    f'{klas3}maat': 'Maatschappijleer',
     f'{klas1}mentor': 'Mentor',
     f'{klas1}na': 'Natuurkunde',
     f'{klas1}ne': 'Nederlands',
