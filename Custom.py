6f25913fe73224e715861784dcb35c2f00a99a8a
LaurensGamet <73340705+LaurensGamet@users.noreply.github.com>
2025-08-16 00:34:03 +0200

    Update Custom.py

--- Custom.py
+++ Custom.py
@@ -38,6 +38,7 @@ Lessen = {
     f'oga4.kua1\, oga5.kua1': 'Kunstgeschiedenis',
     f'oga5.kua1\, oga4.kua1': 'Kunstgeschiedenis',
     f'oga6.kumu1\, oga4.kumu1\, oga5.kumu1\, oh5.kumu1\, oh4.kumu1': 'Kunst (Muziek)',
+    f'oh4.kumu1\, oga5.kumu1\, oga6.kumu1\, oh5.kumu1\, oga4.kumu1': 'Kunst (Muziek)',
     f'{klas1}maat': 'Maatschappijleer',
     f'{klas3}.mentor1': 'Mentor',
     f'{klas3}.mentor3': 'Mentor',
