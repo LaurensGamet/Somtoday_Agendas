a6493a0743dbb3eb60b0e5b1bab32e7ed2920e76
LaurensGamet <laurensgamet@gmail.com>
2025-08-18 23:39:09 +0200

    233918082025

--- Custom.py
+++ Custom.py
@@ -21,7 +21,8 @@ Lokalen = [
     "SUMMARY:veld1 - ",
     "SUMMARY:veld2 - ",
     "SUMMARY:tm018_muziek - ",
-    "SUMMARY:tm222\, tm223\, tm224 - "
+    "SUMMARY:tm222\, tm223\, tm224 - ",
+    "SUMMARY:ut006 - ",
 ]
 
 Lessen = {
@@ -41,6 +42,9 @@ Lessen = {
     f'oh4.kumu1\, oga5.kumu1\, oga6.kumu1\, oh5.kumu1\, oga4.kumu1': 'Kunst (Muziek)',
     f'oh4.kumu1\, oga5.kumu1\, oga4.kumu1\, oga6.kumu1\, oh5.kumu1': 'Kunst (Muziek)',
     f'oh5.kumu1\, oga6.kumu1\, oga4.kumu1\, oga5.kumu1\, oh4.kumu1': 'Kunst (Muziek)',
+    f'oga6.kumu1\, oga5.kumu1\, oga4.kumu1\, oh5.kumu1\, oh4.kumu1': 'Kunst (Muziek)',
+    f'oga4.kumu1\, oh4.kumu1\, oh5.kumu1\, oga5.kumu1\, oga6.kumu1': 'Kunst (Muziek)',
+    f'oh5.kumu1\, oga5.kumu1\, oga6.kumu1\, oh4.kumu1\, oga4.kumu1': 'Kunst (Muziek)',
     f'{klas1}maat': 'Maatschappijleer',
     f'{klas3}.mentor1': 'Mentor',
     f'{klas3}.mentor3': 'Mentor',
