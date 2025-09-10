96689e01e0f98664f559579042a76829845e4fac
LaurensGamet <laurensgamet@gmail.com>
2025-09-10 08:57:58 +0200

    085710092025

--- .Reset.sh
+++ .Reset.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo rm /home/laurens/Somtoday_Agendas/Logs/Laurens.log /home/laurens/Somtoday_Agendas/Logs/Madelief.log
+sudo rm /home/laurens/Somtoday_Agendas/Logs/Laurens.log /home/laurens/Somtoday_Agendas/Logs/Madelief.log /home/laurens/Somtoday_Agendas/Logs/Loukas.log
 
 sudo touch /home/laurens/Somtoday_Agendas/Logs/Laurens.log
 sudo touch /home/laurens/Somtoday_Agendas/Logs/Madelief.log
+sudo touch /home/laurens/Somtoday_Agendas/Logs/Loukas.log
 
 sudo bash /home/laurens/Somtoday_Agendas/.Restart.sh
